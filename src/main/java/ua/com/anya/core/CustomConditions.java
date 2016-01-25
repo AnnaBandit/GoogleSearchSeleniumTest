17a78ad11988f193f54f87341c3486fc9cf1070c
anna_goncharova <anna@bandit-ltd.com>
2016-01-25 02:20:57 +0200

    small changes

--- src/main/java/ua/com/anya/core/CustomConditions.java
+++ src/main/java/ua/com/anya/core/CustomConditions.java
@@ -3,13 +3,10 @@ package ua.com.anya.core;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
 public class CustomConditions{
-
-    public static WebDriver driver;
 
     public static ExpectedCondition<Boolean> listNthElementHasText(final List<WebElement> elements, final int index, final String expectedText) {
         return new ExpectedCondition<Boolean>() {
@@ -19,6 +16,14 @@ public class CustomConditions{
                 } catch (IndexOutOfBoundsException ex) {
                     return false;
                 }
+            }
+        };
+    }
+
+    public static ExpectedCondition<Boolean> sizeOf(final List<WebElement> elements, final int size) {
+        return new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return elements.size() == size;
             }
         };
     }
--- src/test/java/ua/com/anya/GoogleSearchTest.java
+++ src/test/java/ua/com/anya/GoogleSearchTest.java
@@ -3,9 +3,9 @@ package ua.com.anya;
 import org.junit.Test;
 import ua.com.anya.configs.AtGoogleSearchPageWithCreatedPageObjectAndDriver;
 
-import static org.junit.Assert.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 import static ua.com.anya.core.CustomConditions.listNthElementHasText;
+import static ua.com.anya.core.CustomConditions.sizeOf;
 
 public class GoogleSearchTest extends AtGoogleSearchPageWithCreatedPageObjectAndDriver {
 
@@ -13,7 +13,7 @@ public class GoogleSearchTest extends AtGoogleSearchPageWithCreatedPageObjectAnd
     public void testGoogleSearch(){
         googleSearchPage.find("Selenium automates browsers");
         wait.until(listNthElementHasText(googleSearchPage.results, 0, "Selenium automates browsers"));
-        assertTrue(googleSearchPage.results.size() == 10);
+        wait.until(sizeOf(googleSearchPage.results, 10));
 
         googleSearchPage.openLink(0);
         wait.until(titleIs("Selenium - Web Browser Automation"));
